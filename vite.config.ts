aa3631b1a2184f066012137c786aa3c43e1d6cce
YooJinRa <rarastudy@naver.com>
2023-04-15 21:38:40 +0900

    feat: import order

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import eslintPluginImport from 'eslint-plugin-import';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslintPluginImport()],
+  plugins: [react()],
   resolve: {
     alias: [
       { find: '@assets', replacement: '/src/assets' },
